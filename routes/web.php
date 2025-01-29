fd16ca12446449c36d590a285c6a5400d8e23341
Sauradwkna <saura1001@gmail.com>
2025-01-29 22:04:38 +0700

    EPS 12 Post Category

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Category;
 use App\Models\Post;
 use App\Models\User;
 use Illuminate\Support\Arr;
@@ -24,8 +25,13 @@ Route::get('/posts/{post:slug}', function (Post $post) { // route model binding
     return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
-Route::get('/authors/{user}', function (User $user) { // route model binding
-    return view('posts', ['title' => 'Article by ' . $user->name, 'posts' => $user->posts]);
+Route::get('/authors/{user:username}', function (User $user) { // route model binding
+    return view('posts', ['title' => count($user->posts) . ' Article by ' . $user->name, 
+    'posts' => $user->posts]);
+});
+
+Route::get('/categories/{category:slug}', function (Category $category) { // route model binding
+    return view('posts', ['title' => 'Article in: ' . $category->name, 'posts' => $category->posts]);
 });
 
 Route::get('/contact', function () {
